88de5c50f6c65ae56b1c5b84fd3821b209725e1a
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-09-12 23:00:35 -0300

    Criando lógica para receber resposta da api

---
+++ src/20-api-chatgpt/js/script.js
@@ -0,0 +1,57 @@
+const formQuestionChat = document.getElementById("form-question-chat");
+const OPEN_API_KEY = "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";
+
+if (OPEN_API_KEY === "") {
+    document.getElementById("question").innerHTML = "<span style='color: #FF4500;'>Necessário usar uma chave válida!</span>";
+}
+
+const generateResponse = () => {
+    formQuestionChat.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        document.getElementById("btn-question-chat").value = "Pesquisando...";
+
+        let question = document.getElementById("input-question").value;
+
+        document.getElementById("question").innerHTML = question;
+
+        document.getElementById("response").innerHTML = "<span></span>";
+
+        await fetch("https://api.openai.com/v1/completions", {
+            method: "POST",
+
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + OPEN_API_KEY,
+            },
+
+            body: JSON.stringify({
+                model: "text-davinci-003",
+                prompt: question,
+                max_tokens: 2048,
+                temperature: 0.5
+            }),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            document.getElementById("response").innerHTML = data.choices[0].text;
+        })
+        .catch(() => {
+            document.getElementById("response").innerHTML = "Sem resposta";
+        });
+
+        document.getElementById("btn-question-chat").value = "Enviar";
+    });
+}
+
+if (formQuestionChat) {
+    generateResponse();
+}
+
+const cancel = () => {
+    window.location.reload();
+}
+
+document.getElementById("btn-reset-chat").addEventListener("click", cancel);
+
